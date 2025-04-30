cb0d6760f8fd3c2efdcea1470fd4a30a4d93785d
정지은 <o_o9164@naver.com>
2025-04-30 15:29:21 +0900

    Feat: 회원 탈퇴 기능 완료

--- backend/damdam/src/main/java/com/ssafy/damdam/domain/users/controller/UserAuthController.java
+++ backend/damdam/src/main/java/com/ssafy/damdam/domain/users/controller/UserAuthController.java
@@ -1,18 +1,32 @@
 package com.ssafy.damdam.domain.users.controller;
 
+import com.ssafy.damdam.domain.users.service.CustomOAuth2UserService;
+import com.ssafy.damdam.domain.users.service.UserAuthServiceImpl;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
+@RequiredArgsConstructor
 @RestController
+//@RequestMapping("/api/v1/damdam/users")
 public class UserAuthController {
 
+    private final UserAuthServiceImpl userAuthServiceImpl;
+    private final CustomOAuth2UserService customOAuth2UserService;
+
     @GetMapping("")
-    public String index() {
-        return "Hello World!";
+    public ResponseEntity<String> index() {
+        return ResponseEntity.ok().body("index");
     }
 
-    @GetMapping("/logout")
-    public String logout() {
-        return "Logout successful";
+    @DeleteMapping("/leaving")
+    public ResponseEntity<String> leaving() {
+        userAuthServiceImpl.deleteUser();
+        return ResponseEntity.ok().body("탈퇴 요청이 완료되었습니다.");
     }
 }
--- backend/damdam/src/main/java/com/ssafy/damdam/domain/users/service/UserAuthServiceImpl.java
+++ backend/damdam/src/main/java/com/ssafy/damdam/domain/users/service/UserAuthServiceImpl.java
@@ -1,5 +1,8 @@
 package com.ssafy.damdam.domain.users.service;
 
+import com.ssafy.damdam.domain.users.entity.Users;
+import com.ssafy.damdam.domain.users.exception.auth.AuthException;
+import com.ssafy.damdam.domain.users.repository.UserSettingRepository;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,6 +14,8 @@ import com.ssafy.damdam.global.util.user.UserUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
+import static com.ssafy.damdam.domain.users.exception.auth.AuthExceptionCode.AUTH_MEMBER_NOT_FOUND;
+
 @Slf4j
 @Service
 @Transactional
@@ -19,6 +24,18 @@ public class UserAuthServiceImpl extends DefaultOAuth2UserService {
 
 	private final UsersRepository usersRepository;
 	private final UserInfoRepository userInfoRepository;
+	private final UserSettingRepository userSettingRepository;
 	private final UserUtil userUtil;
 
+
+	public void deleteUser() {
+		Users user = userUtil.getUser();
+		if (user == null) {
+			log.error("사용자를 찾을 수 없습니다.");
+			throw new AuthException(AUTH_MEMBER_NOT_FOUND);
+		}
+		userSettingRepository.deleteById(user.getUserId());
+		userInfoRepository.deleteById(user.getUserId());
+		usersRepository.deleteById(user.getUserId());
+	}
 }
