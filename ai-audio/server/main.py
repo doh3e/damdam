05a8dede9438c27851be00aca6d7e8893194b165
BoHyungYu <dbqhgud@sogang.ac.kr>
2025-05-05 16:55:10 +0900

    Feat: 음성 피처 추출 파이프라인 구성

--- ai-audio/server/main.py
+++ ai-audio/server/main.py
@@ -1,7 +1,9 @@
-# main.py
 from fastapi import FastAPI, UploadFile, File, HTTPException, Request
 from fastapi.responses import JSONResponse
 from exceptions import InvalidAudioFormatError
+import model, extractor
+import tempfile
+import os
 
 app = FastAPI()
 
@@ -19,11 +21,24 @@ async def analyze(file: UploadFile=File(...)):
     if content_type not in ALLOWED_AUDIO_TYPES:
         raise InvalidAudioFormatError(f"now allowed file format: {content_type}")
 
-    content = await file.read()
+    # content = await file.read()
+    
+    #---------- 음성 파일 임시 저장 후 피처 추출
+    with tempfile.NamedTemporaryFile(suffix=".wav", delete=False) as tmp:
+        tmp.write(await file.read())
+        tmp.flush()
+        tmp_path = tmp.name
 
+    try:
+        features = extractor.extract(tmp_path)
+    finally:
+        os.remove(tmp_path)
+
+    
     return JSONResponse(content={
         "filename": filename,
         "content_type": content_type,
+        "features": features.shape,
         "message": "file upload complete"
     }, status_code=200)
 
