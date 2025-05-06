7f61630849dfb057da81c1e94bcab32106b4432a
정지은 <o_o9164@naver.com>
2025-05-06 20:43:36 +0900

    Feat: 상담 CD controller 완성
    WebSocket, Redis 연동은 아직임

--- backend/damdam/src/main/java/com/ssafy/damdam/domain/counsels/controller/CounselController.java
+++ backend/damdam/src/main/java/com/ssafy/damdam/domain/counsels/controller/CounselController.java
@@ -1,8 +1,10 @@
 package com.ssafy.damdam.domain.counsels.controller;
 
 import java.net.URI;
+import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -25,6 +27,12 @@ public class CounselController {
 
 	private final CounselService counselService;
 
+	@GetMapping("")
+	public ResponseEntity<List<CounsListDto>> showCounselList() {
+		List<CounsListDto> list = counselService.getCounselList();
+		return ResponseEntity.ok(list);
+	}
+
 	@PostMapping("")
 	public ResponseEntity<CreateCounselResponse> createCounsel() {
 		Long id = counselService.createCounsel();
@@ -33,14 +41,15 @@ public class CounselController {
 			.body(new CreateCounselResponse(id));
 	}
 
-	@GetMapping("/{id}")
-	public ResponseEntity<CounsOutputDto> getCounsel(@PathVariable Long id) {
-		return ResponseEntity.ok(counselService.getCounsel(id));
+	// 상담 방 내역 조회 + 이전 상담 내역 S3에서 뽑아오기 (추후 개발)
+	@GetMapping("/{counsId}")
+	public ResponseEntity<CounsOutputDto> getCounsel(@PathVariable Long counsId) {
+		return ResponseEntity.ok(counselService.getCounsel(counsId));
 	}
 
-	@GetMapping("")
-	public ResponseEntity<CounsListDto> showCounselList() {
-		CounsListDto dto = counselService.getCounselList();
-		return ResponseEntity.ok(dto);
+	@DeleteMapping("/{counsId}")
+	public ResponseEntity<String> deleteCounsel(@PathVariable Long counsId) {
+		counselService.deleteCounsel(counsId);
+		return ResponseEntity.ok("상담이 삭제되었습니다.");
 	}
 }
--- backend/damdam/src/main/java/com/ssafy/damdam/domain/counsels/dto/CounsListDto.java
+++ backend/damdam/src/main/java/com/ssafy/damdam/domain/counsels/dto/CounsListDto.java
@@ -1,4 +1,21 @@
 package com.ssafy.damdam.domain.counsels.dto;
 
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class CounsListDto {
+
+	// 상담 목록을 보여주는 dto
+	private Long counsId;
+	private LocalDateTime createdAt;
+	private LocalDateTime updatedAt;
+
 }
