1d77228d7865d09221fff273d76f29af5cb819fe
김천우 <kcheonu@gmail.com>
2025-05-12 00:44:36 +0000

    Fix: add OAuth2ClientConfig.java

---
+++ backend/damdam/src/main/java/com/ssafy/damdam/global/config/OAuth2ClientConfig.java
@@ -0,0 +1,46 @@
+package com.ssafy.damdam.global.config;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.boot.autoconfigure.security.oauth2.client.OAuth2ClientProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.client.registration.*;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+
+@Configuration
+public class OAuth2ClientConfig {
+
+    private final OAuth2ClientProperties properties;
+
+    public OAuth2ClientConfig(OAuth2ClientProperties properties) {
+        this.properties = properties;
+    }
+
+    @Bean
+    public ClientRegistrationRepository clientRegistrationRepository() {
+        List<ClientRegistration> registrations = this.properties.getRegistration().entrySet().stream()
+            .map(entry -> {
+                String registrationId = entry.getKey();
+                OAuth2ClientProperties.Registration reg = entry.getValue();
+                OAuth2ClientProperties.Provider prov = this.properties.getProvider().get(registrationId);
+
+                return ClientRegistration.withRegistrationId(registrationId)
+                    .clientName(reg.getClientName())
+                    .clientId(reg.getClientId())
+                    .clientSecret(reg.getClientSecret())
+                    .redirectUri(reg.getRedirectUri())
+                    .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+                    .scope(reg.getScope().toArray(new String[0]))
+                    .authorizationUri(prov.getAuthorizationUri())
+                    .tokenUri(prov.getTokenUri())
+                    .userInfoUri(prov.getUserInfoUri())
+                    .userNameAttributeName(prov.getUserNameAttributeName())
+                    .build();
+            })
+            .collect(Collectors.toList());
+
+        return new InMemoryClientRegistrationRepository(registrations);
+    }
+}
