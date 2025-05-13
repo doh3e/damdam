5508d4f17ca4da12b7558c2d17968586032cb9c6
정지은 <o_o9164@naver.com>
2025-05-13 17:00:27 +0900

    Feat: 공지사항 CRUD 개발 완료(예외처리 전)

--- backend/damdam/src/main/java/com/ssafy/damdam/domain/helps/controller/HelpController.java
+++ backend/damdam/src/main/java/com/ssafy/damdam/domain/helps/controller/HelpController.java
@@ -1,10 +1,9 @@
 package com.ssafy.damdam.domain.helps.controller;
 
+import com.ssafy.damdam.domain.helps.dto.NoticeInputDto;
 import com.ssafy.damdam.domain.helps.dto.NoticeOutputDto;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.ssafy.damdam.domain.helps.service.HelpService;
 
@@ -25,5 +24,38 @@ public class HelpController {
 	public ResponseEntity<List<NoticeOutputDto>> getNoticeList() {
 		List<NoticeOutputDto> list = helpService.getNoticeList();
 		return ResponseEntity.ok(list);
+	}
+
+	@PostMapping("/notice")
+	public ResponseEntity<Void> createNotice(
+			@RequestBody NoticeInputDto noticeInputDto
+	) {
+		helpService.createNotice(noticeInputDto);
+		return ResponseEntity.noContent().build();
+	}
+
+	@GetMapping("/notice/{noticeId}")
+	public ResponseEntity<NoticeOutputDto> getNotice(
+			@PathVariable Long noticeId
+	) {
+		NoticeOutputDto notice = helpService.getNotice(noticeId);
+		return ResponseEntity.ok(notice);
+	}
+
+	@PatchMapping("/notice/{noticeId}")
+	public ResponseEntity<Void> updateNotice(
+			@PathVariable Long noticeId,
+			@RequestBody NoticeInputDto noticeInputDto
+	) {
+		helpService.updateNotice(noticeId, noticeInputDto);
+		return ResponseEntity.noContent().build();
+	}
+
+	@DeleteMapping("/notice/{noticeId}")
+	public ResponseEntity<Void> deleteNotice(
+			@PathVariable Long noticeId
+	) {
+		helpService.deleteNotice(noticeId);
+		return ResponseEntity.noContent().build();
 	}
 }
--- backend/damdam/src/main/java/com/ssafy/damdam/domain/helps/service/HelpServiceImpl.java
+++ backend/damdam/src/main/java/com/ssafy/damdam/domain/helps/service/HelpServiceImpl.java
@@ -1,8 +1,12 @@
 package com.ssafy.damdam.domain.helps.service;
 
+import static com.ssafy.damdam.domain.helps.exception.HelpExceptionCode.NOTICE_NOT_FOUND;
 import static com.ssafy.damdam.domain.users.exception.auth.AuthExceptionCode.*;
 
+import com.ssafy.damdam.domain.helps.dto.NoticeInputDto;
 import com.ssafy.damdam.domain.helps.dto.NoticeOutputDto;
+import com.ssafy.damdam.domain.helps.entity.Notice;
+import com.ssafy.damdam.domain.helps.exception.HelpException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,6 +21,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Service
@@ -40,6 +45,54 @@ public class HelpServiceImpl implements HelpService {
 
 	@Override
 	public List<NoticeOutputDto> getNoticeList() {
-		return List.of();
+		return noticeRepository.findAllByOrderByCreatedAtDesc().stream()
+				.map(NoticeOutputDto::fromEntity)
+				.collect(Collectors.toList());
+	}
+
+	@Override
+	@Transactional
+	public void createNotice(NoticeInputDto noticeInputDto) {
+		Users user = validateUser();
+		if (user.getRole().equals("ROLE_USER")) {
+			throw new AuthException(INVALID_MEMBER_ROLE);
+		}
+		noticeRepository.save(Notice.from(noticeInputDto));
+	}
+
+	@Override
+	public NoticeOutputDto getNotice(Long noticeId) {
+		Users user = validateUser();
+		if (user.getRole().equals("ROLE_USER")) {
+			throw new AuthException(INVALID_MEMBER_ROLE);
+		}
+		Notice notice = noticeRepository.findById(noticeId)
+				.orElseThrow(() -> new HelpException(NOTICE_NOT_FOUND));
+
+		return NoticeOutputDto.fromEntity(notice);
+	}
+
+	@Override
+	public void updateNotice(Long noticeId, NoticeInputDto noticeInputDto) {
+		Users user = validateUser();
+		if (user.getRole().equals("ROLE_USER")) {
+			throw new AuthException(INVALID_MEMBER_ROLE);
+		}
+		Notice notice = noticeRepository.findById(noticeId)
+				.orElseThrow(() -> new HelpException(NOTICE_NOT_FOUND));
+
+		notice.updateNotice(noticeInputDto);
+	}
+
+	@Override
+	public void deleteNotice(Long noticeId) {
+		Users user = validateUser();
+		if (user.getRole().equals("ROLE_USER")) {
+			throw new AuthException(INVALID_MEMBER_ROLE);
+		}
+		Notice notice = noticeRepository.findById(noticeId)
+				.orElseThrow(() -> new HelpException(NOTICE_NOT_FOUND));
+
+		noticeRepository.delete(notice);
 	}
 }
