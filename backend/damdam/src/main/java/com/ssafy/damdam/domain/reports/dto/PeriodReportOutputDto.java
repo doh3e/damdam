a3b1de78be6c011a3b0454f48ba58882a1625efc
정지은 <o_o9164@naver.com>
2025-05-19 22:23:02 +0900

    Feat: 기간별 레포트 작업 진행중

--- backend/damdam/src/main/java/com/ssafy/damdam/domain/reports/dto/PeriodReportOutputDto.java
+++ backend/damdam/src/main/java/com/ssafy/damdam/domain/reports/dto/PeriodReportOutputDto.java
@@ -1,5 +1,11 @@
 package com.ssafy.damdam.domain.reports.dto;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+import com.ssafy.damdam.domain.counsels.dto.CounselingDto;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -11,5 +17,15 @@ import lombok.NoArgsConstructor;
 @Builder
 public class PeriodReportOutputDto {
 
-    private Long pReportId;
+	private Long pReportId;
+	private String pReportTitle;
+	private LocalDate startDate;
+	private LocalDate endDate;
+	private List<CounselingDto> counselings;
+	private String advice;
+	private String compliment;
+	private String summary;
+	private String worry;
+	private LocalDateTime createdAt;
+	private int counselCount;
 }
