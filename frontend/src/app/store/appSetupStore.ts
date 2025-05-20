a665f5bb9aebc8ecd3332af4c07850821d23d45b
서준호 <sjh2395@gmail.com>
2025-05-21 04:07:31 +0900

    Fix: 문제 확인을 위한 녹음, 인코더 라이브러리 제거

--- frontend/src/app/store/appSetupStore.ts
+++ frontend/src/app/store/appSetupStore.ts
@@ -1,11 +1,19 @@
 import { create } from 'zustand';
 
-interface AppSetupState {
-  isWavEncoderReady: boolean;
-  setWavEncoderReady: (isReady: boolean) => void;
+export interface AppSetupState {
+  isSocketConnected: boolean;
+  setIsSocketConnected: (isConnected: boolean) => void;
+  isWavEncoderReady: boolean; // WAV 인코더 준비 상태
+  setWavEncoderReady: (isReady: boolean) => void; // WAV 인코더 준비 상태 설정 함수
 }
 
+/**
+ * Zustand 스토어를 생성합니다.
+ * 이 스토어는 애플리케이션 설정과 관련된 상태를 관리합니다.
+ */
 export const useAppSetupStore = create<AppSetupState>((set) => ({
-  isWavEncoderReady: false, // 초기값은 false
+  isSocketConnected: false,
+  setIsSocketConnected: (isConnected) => set({ isSocketConnected: isConnected }),
+  isWavEncoderReady: true, // 초기값을 true로 변경하여 AudioEncoderInitializer 의존성 임시 제거
   setWavEncoderReady: (isReady) => set({ isWavEncoderReady: isReady }),
 }));
